f470b3bea61d37b26310132187dd569cd78f69f0
minji99710 <alswl99710@gmail.com>
2023-06-28 09:53:33 +0900

    Feat: data mutation 타입스크립트화
    Co-authored-by: yebin <bin000527@users.noreply.github.com>

--- src/hooks/useCommentsMutation.ts
+++ src/hooks/useCommentsMutation.ts
@@ -1,19 +1,42 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import useDataMutation from './useDataMutaiton';
 import { commentQueryKey } from '../constants/index';
 
 const url = `/api/comments`;
 
-const useCommentsMutation = ({ storeId, currentPage }) => {
-  const { mutate: addComment } = useDataMutation({
+interface CommentsMutationProps {
+  storeId: string;
+  currentPage: number;
+}
+
+export interface CommentType {
+  storeId: string;
+  content: string;
+  email: string;
+  isCertified: boolean;
+  nickname: string;
+  commentId?: string;
+}
+
+export interface CommentsType {
+  data: CommentType[];
+  totalPages: number;
+}
+
+const useCommentsMutation = ({ storeId, currentPage }: CommentsMutationProps) => {
+  const { mutate: addComment } = useDataMutation<CommentType, CommentsType>({
     mutationFn: newComment => axios.post(url, newComment),
-    onMutate: newComment => comments => [newComment, ...comments.data],
+    onMutate: newComment => comments => {
+      if (comments) return { ...comments, data: [newComment, ...comments.data] };
+    },
     queryKey: [...commentQueryKey, storeId, currentPage],
   });
 
-  const { mutate: deleteComment } = useDataMutation({
+  const { mutate: deleteComment } = useDataMutation<string, CommentsType>({
     mutationFn: commentId => axios.delete(`${url}/${commentId}`),
-    onMutate: id => comments => comments.data.filter(comment => comment.commentId !== id),
+    onMutate: id => comments => {
+      if (comments) return { ...comments, data: comments.data.filter(comment => comment.commentId !== id) };
+    },
     queryKey: [...commentQueryKey, storeId, currentPage],
   });
 
--- src/hooks/useDataMutaiton.ts
+++ src/hooks/useDataMutaiton.ts
@@ -1,38 +1,56 @@
-import { useQueryClient, useMutation, MutationFunction } from '@tanstack/react-query';
+import {
+  useQueryClient,
+  useMutation,
+  MutationFunction,
+  UseMutationResult,
+  QueryKey,
+  Updater,
+} from '@tanstack/react-query';
+import { AxiosError } from 'axios';
+import { ArchiveType } from './useArchivesMutation';
+import type { CommentType, CommentsType } from './useCommentsMutation';
 
-interface MutationPropsType<TData, TVariables> {
-  mutationFn: (variables: TVariables) => Promise<TData>;
-  onMutate: (variables: TVariables) => (variable?: TData) => TData;
-  queryKey: Array<string | number>;
+// interface MutationPropsType<TData, TVariables> {
+//   mutationFn: MutationFunction<TData, TVariables>;
+//   onMutate: (variables: TVariables) => (variable?: TData) => Promise<TData>;
+//   queryKey: Array<string | number>;
+// }
+
+interface MutationPropsType<A, C> {
+  mutationFn: MutationFunction<C, A>;
+  // onMutate: (variables: CommentType) => (variable: CommentsType) => CommentType[];
+  // onMutate: <T extends CommentType, C>(variables: T) => (variable: C) => T[];
+  // onMutate: (variables: A) => (variable: B) => C;
+  onMutate: (variables: A) => Updater<C | undefined, C | undefined>;
+  // onMutate: <T>(variables: T) => (variable: T[]) => T[];
+  queryKey: QueryKey;
 }
 
-const useDataMutation = <TData, TVariables>({
-  mutationFn,
-  onMutate: expected,
-  queryKey,
-}: MutationPropsType<TData, TVariables>) => {
+const useDataMutation = <A, C>({ mutationFn, onMutate: expected, queryKey }: MutationPropsType<A, C>) => {
   const queryClient = useQueryClient();
 
-  return useMutation({
+  return useMutation<C, AxiosError, A, C>({
     mutationFn,
-    onMutate(variable) {
+    onMutate(variable: A) {
       // 낙관적 업데이트를 덮어쓰지 않도록 refetch 모두 취소
       queryClient.cancelQueries();
       // 롤백 위한 이전 상태 저장
-      const previousData = queryClient.getQueryData(queryKey);
+      const previousData = queryClient.getQueryData<C>(queryKey);
       console.log('previous: ', queryClient.getQueryData(queryKey));
       // 낙관적 업데이트
       queryClient.setQueryData(queryKey, expected(variable));
       // 반환값은 onError의 context로 사용
-      return { previousData };
+      return previousData;
     },
-    onError(error, newComment, context) {
+
+    onError(error: AxiosError, variables: A, previousData: C) {
       // 서버 요청 실패 시 롤백
-      if (context !== undefined) {
-        queryClient.setQueryData(queryKey, context.previousData);
+      if (previousData !== undefined) {
+        queryClient.setQueryData(queryKey, previousData);
       }
     },
-    onSuccess() {
+
+    onSuccess(data: C, variables: A, previousData: C) {
       // 서버 요청 실패 또는 성공 시 refetch하기
       // 관련 업데이트가 되는 동안 loading 상태 유지
       return queryClient.invalidateQueries(queryKey);
